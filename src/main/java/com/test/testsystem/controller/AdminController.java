86de7e60f2247c7d3b46873669f6b762f7b79fdf
jinyi <925023204@qq.com>
2019-12-15 22:32:44 +0800

    增删改查

--- src/main/java/com/test/testsystem/controller/AdminController.java
+++ src/main/java/com/test/testsystem/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.test.testsystem.service.AdminService;
 import com.test.testsystem.utils.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,7 +16,7 @@ public class AdminController {
 
     @RequestMapping("/adminLogin")
     public String adminLogin(){
-        return "admin_login";
+        return "sign";
     }
 
     @RequestMapping("/systemList")
